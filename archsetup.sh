f462104a8080b66f1330c629b8cac4d387f90e8b
Matt Riechman-Bennett <me@mattriechmanbennett.com>
2022-10-02 21:24:32 -0500

    Save

--- archsetup.sh
+++ archsetup.sh
@@ -17,7 +17,7 @@ echo "Please type the name of the system encrypted partition"
 read SYSNAME
 echo "(i)ntel or (a)md?"
 read PROCESSOR
-if $PROCESSOR=="i"
+if ["$PROCESSOR"=="i"]
 then
 	PROCPAC="intel-ucode"
 else
