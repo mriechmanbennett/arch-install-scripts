d1ebfedf94edf7b70bea2ffcff61b281597bfadf
Matt Riechman-Bennett <me@mattriechmanbennett.com>
2023-12-07 21:17:02 -0600

    Update packages for archiso

---
+++ archiso-build.sh
@@ -0,0 +1,2 @@
+mkdir -p archiso-build-files
+mkarchiso -v -w archiso-build-files/ -o archiso-build-files/ archiso-config/
--- archiso-config/profiledef.sh
+++ archiso-config/profiledef.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034
 
 iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_label="ARCH_CUSTOM_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Arch Linux <https://archlinux.org>"
 iso_application="Arch Linux Live/Rescue CD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
